5164ce190a400311f80370e5db7bc4c33c65ae8f
amcd.dev <105273458+amcd-dev@users.noreply.github.com>
2022-11-22 10:05:15 +1100

    added cors package for troubleshooting

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,4 +1,5 @@
 import sql from "../../lib/db";
+import NextCors from "nextjs-cors";
 
 async function getStrength (uid, day) {
     const activity = await sql `
@@ -10,6 +11,14 @@ async function getStrength (uid, day) {
 export default async function handler(req, res) {
     console.log('Received time line fetch')
     // console.log('>>> logging req.query', req.query)
+    await NextCors(req, res, {
+        // Options
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200,
+    });
+
+
     const uid = req.query.uid
     const day = req.query.day
     const result = await getStrength(uid, day)
